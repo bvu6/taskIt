9201b7a106fb05686d69c38694478cda8ee4e7c7
LD4400 <l_delrosario@u.pacific.edu>
2022-03-27 12:18:33 -0700

    Added junit test

---
+++ src/test/java/edu/pacific/comp55/starter/test1.java
@@ -0,0 +1,14 @@
+package edu.pacific.comp55.starter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test1 {
+
+	@Test
+	void test() {
+		assertEquals(0, 0);
+	}
+
+}
