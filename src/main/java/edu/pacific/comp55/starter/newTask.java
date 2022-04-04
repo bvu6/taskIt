6d18dc1cccfc9401bf64f4685f9c0e17779812bf
Benjamin <b_vu6@u.pacific.edu>
2022-04-04 00:16:47 -0700

    Added titles and project name textfields

--- src/main/java/edu/pacific/comp55/starter/newTask.java
+++ src/main/java/edu/pacific/comp55/starter/newTask.java
@@ -1,6 +1,7 @@
 package edu.pacific.comp55.starter;
 
 import java.awt.event.ActionEvent;
+import java.awt.BorderLayout; 
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
@@ -13,17 +14,26 @@ public class newTask extends JFrame
 {
        JPanel jp = new JPanel();
        JLabel jl = new JLabel();
-       JTextField jt = new JTextField(30);
-       JButton jb = new JButton("Enter");
+       JLabel projectOutput = new JLabel(); 
+       JLabel titleLabel = new JLabel("Title:"); 
+       JLabel projectLabel = new JLabel ("Project Label:"); 
+       JTextField jt = new JTextField(20);
+       JTextField projectText = new JTextField(20);
+       JButton jb = new JButton("Enter\n");
+       JButton projectB = new JButton ("Enter"); 
 
        public newTask()
        {
+    	   
               setTitle("Create New Task");
               setVisible(true);
               setSize(400, 200);
               setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-              jp.add(jt);
+              //jp.add(jt);
+              titleLabel.setBounds(10,20,80,25); 
+              jp.add(titleLabel); 
+              jt.setBounds(100,20,165,25); 
+              jp.add(jt); 
 
 
               jt.addActionListener(new ActionListener()
@@ -46,9 +56,35 @@ public class newTask extends JFrame
               });
               
               jp.add(jl); //prints out the user input
+              
+              projectLabel.setBounds(10,50,80,20); 
+              jp.add(projectLabel); 
+              projectText.setBounds(100,50,165,25); 
+              jp.add(projectText); 
+              projectText.addActionListener(new ActionListener()
+              {
+                     public void actionPerformed(ActionEvent e)
+                     {
+                           String input = projectText.getText();
+                           projectOutput.setText(input); 
+                     }
+              });
+
+              jp.add(projectB);
+              projectB.addActionListener(new ActionListener()
+              {
+                      public void actionPerformed(ActionEvent e)
+                      {
+                             String input = projectText.getText();
+                             projectOutput.setText(input);
+                      }
+              });
+              
               add(jp);
+              
 
        }
+
 
        public static void main(String[] args)
        {
